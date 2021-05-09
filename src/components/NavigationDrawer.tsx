ed8fb655d34c871777f8179a382529680ead90f4
woobuntu <boutime2017@gmail.com>
2021-05-09 23:30:58 +0900

    NavigationDrawer컴포넌트에 navigating을 담당하는 pushTo함수 구현

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -3,12 +3,23 @@ import { Drawer } from "@material-ui/core";
 import { TreeView, TreeItem } from "@material-ui/lab";
 import { ExpandMore, ChevronRight } from "@material-ui/icons";
 import { navigation_menu } from "../dummy";
+import { useHistory } from "react-router-dom";
 
-interface PropsFunction {
+interface propsFunction {
   closeDrawer: () => void;
 }
 
-const NavigationDrawer = ({ closeDrawer }: PropsFunction) => {
+interface pushToParams {
+  category: string;
+  destination: string;
+}
+
+const NavigationDrawer = ({ closeDrawer }: propsFunction) => {
+  const history = useHistory();
+  const pushTo = ({ category, destination }: pushToParams) => {
+    history.push(`/${category}/${destination}`);
+    closeDrawer();
+  };
   return (
     <Drawer variant="temporary" anchor="left" onClose={closeDrawer} open>
       <TreeView
@@ -16,15 +27,20 @@ const NavigationDrawer = ({ closeDrawer }: PropsFunction) => {
         defaultCollapseIcon={<ExpandMore />}
         defaultExpandIcon={<ChevronRight />}
       >
-        {navigation_menu.map(({ nodeId, label, children }) => (
+        {navigation_menu.map(({ nodeId, label, children, link }) => (
           <TreeItem key={nodeId} nodeId={nodeId} label={label}>
-            {children.map(({ nodeId: childNodeId, label: childLabel }) => (
-              <TreeItem
-                key={childNodeId}
-                nodeId={childNodeId}
-                label={childLabel}
-              />
-            ))}
+            {children.map(
+              ({ nodeId: childNodeId, label: childLabel, link: childLink }) => (
+                <TreeItem
+                  key={childNodeId}
+                  nodeId={childNodeId}
+                  label={childLabel}
+                  onClick={() =>
+                    pushTo({ category: link, destination: childLink })
+                  }
+                />
+              )
+            )}
           </TreeItem>
         ))}
       </TreeView>
