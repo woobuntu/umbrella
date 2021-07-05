6288d582ed035444cb1f91609a2c43ff319064aa
IN UH LEE <inuhlee@INui-MacBookPro.local>
2021-07-05 16:00:26 +0900

    spring boot project 초기화

---
+++ server/src/test/java/kr/withus1030/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.withus1030.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
