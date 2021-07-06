f3a8771d3577459cf08243a6180c7908d7989719
woobuntu <boutime2017@gmail.com>
2021-07-06 14:33:57 +0900

    MetaService interface 생성

---
+++ server/src/main/java/kr/withus1030/server/service/MetaService.java
@@ -0,0 +1,10 @@
+package kr.withus1030.server.service;
+
+import kr.withus1030.server.entity.Meta;
+
+public interface MetaService {
+
+    public Meta get();
+
+    public void update(Meta meta);
+}
