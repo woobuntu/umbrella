a7440661542d3e9708170283f822bd0f622e376c
woobuntu <boutime2017@gmail.com>
2021-07-06 12:36:32 +0900

    Contact entity 생성

---
+++ server/src/main/java/kr/withus1030/server/entity/Contact.java
@@ -0,0 +1,75 @@
+package kr.withus1030.server.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="contact")
+public class Contact {
+
+    @Id
+    @OneToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name="name")
+    private Meta meta;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="mail")
+    private String mail;
+
+    @Column(name="phone")
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Contact(Meta meta, String address, String mail, String phone) {
+        this.meta = meta;
+        this.address = address;
+        this.mail = mail;
+        this.phone = phone;
+    }
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Meta meta) {
+        this.meta = meta;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "meta=" + meta +
+                ", address='" + address + '\'' +
+                ", mail='" + mail + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
