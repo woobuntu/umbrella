bfad9f1b001fb8c423377e267fb9abf0bf695be6
woobuntu <boutime2017@gmail.com>
2021-07-06 13:00:18 +0900

    Performance entity 생성

---
+++ server/src/main/java/kr/withus1030/server/entity/Performance.java
@@ -0,0 +1,64 @@
+package kr.withus1030.server.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "performance")
+public class Performance {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "timestamp")
+    private Date timestamp;
+
+    public Performance() {
+    }
+
+    public Performance(Long id, String title, Date timestamp) {
+        this.id = id;
+        this.title = title;
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Performance{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
