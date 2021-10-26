8bc9f409009a06a82730b472c8f4b7d7916c6b4b
in uh, lee <boutime2017@gmail.com>
2021-10-26 15:32:42 +0900

    graphql setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      // code first approach를 통해 schema 파일 생성
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // 스키마를 사전편찬순으로 정렬
+      sortSchema: true,
+      // apollo-server-fastify는 복수의 graphql 엔드포인트를 허용하기 위해서
+      // healthCheck를 해제해주어야 한다.
+      // https://docs.nestjs.com/graphql/quick-start#multiple-endpoints
+      disableHealthCheck: true,
+      // graphql-playground 대신 apollo sandbox를 graphql ide로 사용
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
