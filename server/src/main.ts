2a817140787288aed4ceb38709b84258e33bbbb7
in uh, lee <boutime2017@gmail.com>
2021-10-26 15:01:36 +0900

    fastify setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
+// https://docs.nestjs.com/techniques/performance#performance-fastify
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+  await app.listen(3000, '0.0.0.0');
+  // fastify는 기본적으로 localhost 127.0.0.1 interface에서만 수신이 가능하다.
+  // 다른 IPv4 interface에서도 수신할 수 있게 하기 위해 '0.0.0.0'을 listen메서드의 두 번째 인자로 전달해주는 것
 }
 bootstrap();
