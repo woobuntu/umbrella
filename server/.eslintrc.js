93f907a67cd9d7b62694668080d9e6fc71e271bc
in uh, lee <boutime2017@gmail.com>
2021-10-26 13:42:48 +0900

    eslint parsing error 해결

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,6 +2,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    // 이 project 경로는 현재 working directory로 귀결되는데,
+    // working directory는 현재 vs코드가 열린 디렉토리이다.
+    // 즉, nest프로젝트보다 상위의 디렉토리에서 열린 경우
+    // working directory에서 tsconfig.json파일을 찾을 수 없기에
+    // eslint가 parsing error를 반환하게 된다.
+    // 따라서 아래와 같이 tsconfig의 경로를 .eslintrc.js가 위치한 상대경로로 지정해주어
+    // vscode의 working directory와 상관없이 tsconfig를 찾을 수 있게끔 한 것
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
