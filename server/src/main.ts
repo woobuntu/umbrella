0579548e532bb65711bde5f7d4deffc9a4a1f72b
in uh, lee <boutime2017@gmail.com>
2021-10-26 18:51:20 +0900

    port 변경

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
       logger: true,
     }),
   );
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(5000, '0.0.0.0');
   // fastify는 기본적으로 localhost 127.0.0.1 interface에서만 수신이 가능하다.
   // 다른 IPv4 interface에서도 수신할 수 있게 하기 위해 '0.0.0.0'을 listen메서드의 두 번째 인자로 전달해주는 것
 }
