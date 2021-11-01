5d4e502e20c359f5a9fe04a1bd1cc6f45afe5970
in uh, lee <boutime2017@gmail.com>
2021-11-01 15:19:10 +0900

    apollo client cookie setup

--- material-kit-pro-react-v1.10.0/src/graphql/client.js
+++ material-kit-pro-react-v1.10.0/src/graphql/client.js
@@ -1,8 +1,15 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+
+// https://www.apollographql.com/docs/react/networking/authentication/#cookie
+const httpLink = createHttpLink({
+  uri: "http://localhost:5000/graphql",
+  credentials: "include",
+});
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
+  link: httpLink,
 });
 
 export default client;
