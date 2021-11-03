e9e2a5528d2128f120f9ef8d85f4be7dd4ea6694
in uh, lee <boutime2017@gmail.com>
2021-11-03 12:47:57 +0900

    SetCookieInterceptor 리팩토링
    - 기존 콜백 패턴을 observer 패턴으로 변경

--- server/src/interceptors/set-cookie.interceptor.ts
+++ server/src/interceptors/set-cookie.interceptor.ts
@@ -7,7 +7,17 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { FastifyReply } from 'fastify';
-import { map, Observable, tap } from 'rxjs';
+import {
+  catchError,
+  concatMap,
+  from,
+  map,
+  Observable,
+  of,
+  tap,
+  timer,
+} from 'rxjs';
+import { promisify } from 'util';
 
 // https://stackoverflow.com/questions/63195571/unable-to-set-cookie-in-nestjs-graphql
 @Injectable()
@@ -21,37 +31,56 @@ export class SetCookieInterceptors implements NestInterceptor {
     isAuthenticated: boolean;
   }> {
     return next.handle().pipe(
-      tap((userInfoFromDB) => {
-        // 인증된 경우에만 sessionId 반환
-        if (userInfoFromDB) {
-          const ctx = GqlExecutionContext.create(context);
+      concatMap((userInfoFromDB) => {
+        // naverSignIn mutation이 유저 정보를 반환하지 않으면 false반환
+        if (!userInfoFromDB) return of(false);
 
-          const { request } = ctx.getContext();
+        // naverSignIn mutation이 유저 정보를 반환하면 session setup
+        const ctx = GqlExecutionContext.create(context);
+        const { request } = ctx.getContext();
+        const { session, sessionStore } = request;
+        const { sessionId, cookie } = session;
+        session.user = userInfoFromDB;
 
-          const { session, sessionStore } = request;
-
-          const { sessionId, cookie } = session;
-
-          session.user = userInfoFromDB;
-
-          sessionStore.set(sessionId, session, () => {
+        return from(
+          promisify(sessionStore.set.bind(sessionStore))(sessionId, session),
+        ).pipe(
+          tap(() => {
             const reply: FastifyReply = ctx.getContext().reply;
-
             reply.setCookie('JSESSIONID', sessionId, cookie);
 
             const sessionDuration = this.configService.get('SESSION_DURATION');
-
-            setTimeout(() => {
-              sessionStore.destroy(sessionId, () => {
-                // 이렇게 콜백을 설정하면 memory leak이 발생하는 것이 아닌가...
+            timer(sessionDuration)
+              .pipe(
+                concatMap(() =>
+                  from(
+                    promisify(sessionStore.destroy.bind(sessionStore))(
+                      sessionId,
+                    ),
+                  ),
+                ),
+              )
+              .subscribe({
+                next: () =>
+                  console.log(
+                    `sessionId ${sessionId} is Destroyed!`,
+                    sessionStore,
+                  ),
+                error: (error) =>
+                  console.log('Session Destroy Error : ', error),
               });
-            }, sessionDuration);
-          });
-        }
+          }),
+          // 에러 없이 cookie와 세션 유효기간 설정했으면 true 반환
+          map(() => true),
+        );
       }),
-      map((userInfoFromDB) => ({
-        isAuthenticated: userInfoFromDB ? true : false,
+      map((isAuthenticated) => ({
+        isAuthenticated,
       })),
+      catchError((error) => {
+        console.log(error);
+        return of({ isAuthenticated: false });
+      }),
     );
   }
 }
