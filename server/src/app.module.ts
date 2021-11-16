99d652aeb6357af962733ac1424e204a6f86828d
in uh, lee <boutime2017@gmail.com>
2021-11-16 17:26:34 +0900

    환경별로 cors 허용 url 설정

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
 import {
@@ -44,24 +44,26 @@ import {
     CatalogModule,
     BasketModule,
     StaticModule,
-    GraphQLModule.forRoot({
-      // code first approach를 통해 schema 파일 생성
-      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
-      // 스키마를 사전편찬순으로 정렬
-      sortSchema: true,
-      // graphql-playground 대신 apollo sandbox를 graphql ide로 사용
-      // 배포된 graphql server의 documentation을 apollo sandbox가 인지하지 못하는데,
-      // 쿼리는 정상적으로 동작하니 당황하지 말자
-      playground: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault],
-      // https://docs.nestjs.com/security/cors#getting-started
-      cors: {
-        // origin: 'http://localhost:3000',
-        origin: true,
-        credentials: true,
-      },
-      // https://stackoverflow.com/questions/63195571/unable-to-set-cookie-in-nestjs-graphql
-      context: ({ request, reply }) => ({ request, reply }),
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        // code first approach를 통해 schema 파일 생성
+        autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+        // 스키마를 사전편찬순으로 정렬
+        sortSchema: true,
+        // graphql-playground 대신 apollo sandbox를 graphql ide로 사용
+        // 배포된 graphql server의 documentation을 apollo sandbox가 인지하지 못하는데,
+        // 쿼리는 정상적으로 동작하니 당황하지 말자
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault],
+        // https://docs.nestjs.com/security/cors#getting-started
+        cors: {
+          origin: configService.get<string>('environment.clientUrl'),
+          credentials: true,
+        },
+        // https://stackoverflow.com/questions/63195571/unable-to-set-cookie-in-nestjs-graphql
+        context: ({ request, reply }) => ({ request, reply }),
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
