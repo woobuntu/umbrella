98259d29306fa62cce44c7a0e64d608e0b46eec4
in uh, lee <boutime2017@gmail.com>
2021-11-17 23:07:56 +0900

    쿠키 유효시간 설정 제거

--- material-kit-pro-react-v1.10.0/src/graphql/client.js
+++ material-kit-pro-react-v1.10.0/src/graphql/client.js
@@ -1,34 +1,12 @@
-import {
-  ApolloClient,
-  ApolloLink,
-  concat,
-  createHttpLink,
-  InMemoryCache,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const uri = process.env.REACT_APP_API_URL;
 
-// https://www.apollographql.com/docs/react/networking/authentication/#cookie
-const httpLink = createHttpLink({
-  uri,
-  credentials: "include",
-});
-
-// // https://www.apollographql.com/docs/react/networking/advanced-http-networking/#customizing-request-logic
-const setDateInHeader = new ApolloLink((operation, forward) => {
-  operation.setContext(({ headers = {} }) => ({
-    headers: {
-      ...headers,
-      timestamp: Date.now(),
-    },
-  }));
-  return forward(operation);
-});
-
 // https://www.apollographql.com/docs/react/networking/basic-http-networking/#including-credentials-in-requests
 const client = new ApolloClient({
+  uri,
   cache: new InMemoryCache(),
-  link: concat(setDateInHeader, httpLink),
+  credentials: "include",
 });
 
 export default client;
