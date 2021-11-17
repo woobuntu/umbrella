871451023f3f36c0d7755f97cdaacbefb50edb3a
in uh, lee <boutime2017@gmail.com>
2021-11-17 19:25:17 +0900

    불필요한 코드 삭제

--- server/src/interceptors/set-cookie.interceptor.ts
+++ server/src/interceptors/set-cookie.interceptor.ts
@@ -45,8 +45,6 @@ export class SetCookieInterceptor implements NestInterceptor {
           .pipe(
             tap(() => {
               const reply: FastifyReply = ctx.getContext().reply;
-              console.log(0, request);
-              console.log(1, session);
 
               const sessionDuration =
                 this.configService.get<SessionConfig>('session.duration');
