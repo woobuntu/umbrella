0b617d764696c91bbc503faefb79d1deb3615795
woobuntu <boutime2017@gmail.com>
2022-04-19 00:10:30 +0900

    minor fix

--- material-kit-pro-react-v1.10.0/src/pages/Mall.js
+++ material-kit-pro-react-v1.10.0/src/pages/Mall.js
@@ -11,6 +11,10 @@ import InfoArea from "components/InfoArea/InfoArea";
 import { Eco } from "@material-ui/icons";
 import { FaHands } from "react-icons/fa";
 import { GiReceiveMoney } from "react-icons/gi";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import ViewCompactIcon from "@material-ui/icons/ViewCompact";
+import RedeemIcon from "@material-ui/icons/Redeem";
 const useStyles = makeStyles(mallStyle);
 
 export default function Mall() {
@@ -74,29 +78,39 @@ export default function Mall() {
                 </div>
               </GridItem>
               <GridItem xs={12} sm={6} md={6}>
-                <h2 className={classes.title}>
-                  고객님께 드리는 함께쓰는우산의 약속
-                </h2>
+                <h3 className={classes.title} style={{ marginBottom: 0 }}>
+                  장애인 연계고용제도를 활용할 수 있는 사업장입니다.
+                </h3>
+                <h4 className={classes.title} style={{ marginTop: 0 }}>
+                  (고용분담금 50% 감면혜택)
+                </h4>
                 <InfoArea
                   className={classes.infoArea}
-                  icon={Eco}
-                  title="양질의 고급 재료만을 사용합니다"
-                  description="방부제는 일체 사용하지 않습니다"
+                  icon={CardGiftcardIcon}
+                  title="거래처, 사내직원, 명절, 기념일 선물용으로 좋습니다."
+                  description=""
                   iconColor="success"
                 />
                 <InfoArea
                   className={classes.infoArea}
-                  icon={FaHands}
-                  title="모든 제조과정은 정성을 가득 담은 전통적 수작업입니다"
-                  description="소중한 우리 가족과 이웃을 아끼는 마음으로 정성껏 만듭니다"
+                  icon={ReceiptIcon}
+                  title="요청시 기부영수증을 발행해 드립니다."
+                  description=""
                   iconColor="warning"
                 />
                 <InfoArea
                   className={classes.infoArea}
-                  icon={GiReceiveMoney}
-                  title="모든 수익금은 발달장애인의 일자리 지원사업에 사용됩니다"
-                  description=""
+                  icon={ViewCompactIcon}
+                  title="선물은 고객의 주문에 따라 가격별로 구성을 다양하게 할 수 있습니다."
+                  description="031-342-1030(유선문의)"
                   iconColor="info"
+                />
+                <InfoArea
+                  className={classes.infoArea}
+                  icon={RedeemIcon}
+                  title="선물상자는 수급사정에 따라 변경될 수 있습니다."
+                  description=""
+                  iconColor="rose"
                 />
               </GridItem>
             </GridContainer>
